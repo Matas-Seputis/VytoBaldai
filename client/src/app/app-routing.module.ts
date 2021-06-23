71f125364c644b3beebc91af9905c6b284403360
Matas Seputis <matasseputis@gmail.com>
2021-06-23 20:08:53 +0200

    Handling Errors in Angular

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { PagrindinisComponent } from './home/pagrindinis.component';
-import { KatalogasComponent } from './shop/katalogas.component';
-import { ProduktoSpecifikacijaComponent } from './shop/produkto-specifikacija/produkto-specifikacija.component';
 
 const routes: Routes = [
   {path: '', component: PagrindinisComponent},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
   {path: 'katalogas', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
 @NgModule({
