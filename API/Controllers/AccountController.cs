e416f4c9ee5de327163aff47d4bef0c88156ee0c
Matas Seputis <matasseputis@gmail.com>
2021-06-26 15:17:08 +0200

    Applying API Validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,7 +44,7 @@ namespace API.Controllers
     }
 
     [HttpGet("emailexists")]
-    public async Task<ActionResult<bool>> ChechEmailExistsAsync([FromQuery] string email)
+    public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
     {
         return await _userManager.FindByEmailAsync(email) != null;
     }
@@ -56,6 +56,21 @@ namespace API.Controllers
         var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
 
         return _mapper.Map<Address, AddressDto>(user.Address);
+    }
+
+    [Authorize]
+    [HttpPut("address")]
+    public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
+    {
+        var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
+
+        user.Address = _mapper.Map<AddressDto, Address>(address);
+
+        var result = await _userManager.UpdateAsync(user);
+
+        if (result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+
+        return BadRequest("Problem updating the user");
     }
 
     [HttpPost("login")]
@@ -80,6 +95,11 @@ namespace API.Controllers
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
+        if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+        {
+            return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{"Email address is in use"}});
+        }
+
         var user = new AppUser
         {
             DisplayName = registerDto.DisplayName,
--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class AddressDto
     {
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
     }
 }
